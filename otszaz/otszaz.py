edfcacab3ddb629fd66a11d4b12fd89e36ea5c08
Kovács Patrik <kovacsp20@ADY-NAGYATAD.SULINET.HU>
2022-03-07 08:42:10 +0100

    otodik

--- otszaz/otszaz.py
+++ otszaz/otszaz.py
@@ -11,4 +11,12 @@ f.close()
 print("2.feladat")
 print("A fizetések száma: " + str(kosar.count("F")))
 
-print("Az első vásárló"+str(kosar.index("F"))+"darab árucikek vásárlása")
+print("Az első vásárló "+str(kosar.index("F"))+" darab árucikek vásárlása")
+
+sorszam = int(input("Vásárlás sorszéma "))
+arnev = input("Árucik neve ")
+darab = int(input("Darabszám "))
+
+aruindex = kosar.index(arnev)
+darablista = kosar[:aruindex]
+print(darablista)
