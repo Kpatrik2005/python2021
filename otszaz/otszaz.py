e4de164f198063af92618f03da26ff390baf9e2c
Kovács Patrik <kovacsp20@ADY-NAGYATAD.SULINET.HU>
2022-03-21 08:25:10 +0100

    7.feladatvége

--- otszaz/otszaz.py
+++ otszaz/otszaz.py
@@ -1,22 +1,91 @@
-#megszámolás de a több is egynek számít
+def ertek(darab):
+    if darab == 1:
+        return 500
+    else:
+        return darab * 400 + 150
+
 f = open("penztar.txt")
+
 kosar = []
-#szoveg=f.read()
+#szoveg = f.read()
 #print(szoveg)
-#print(f.readline())
 for sor in f:
     kosar.append(sor.strip())
+
 f.close()
 
-print("2.feladat")
 print("A fizetések száma: " + str(kosar.count("F")))
 
-print("Az első vásárló "+str(kosar.index("F"))+" darab árucikek vásárlása")
+print("Az első vásárló " + str(kosar.append("F")) + " darab árucikket vásárolt.")
 
-sorszam = int(input("Vásárlás sorszéma "))
-arnev = input("Árucik neve ")
-darab = int(input("Darabszám "))
+print("A fizetések száma: " + str(kosar.count("F")))
 
-aruindex = kosar.index(arnev)
-darablista = kosar[:aruindex]
-print(darablista)
+print("Az első vásárló " + str(kosar.index("F")) + " darab árucikket vásárolt.")
+
+sorszam = int(input("Vásárlás sorszáma: "))
+arunev = input("Árucikk neve: ")
+darab = int(input("Darabszám: "))
+
+#A termék első indexe
+aruIndex = kosar.index(arunev)
+darabLista = kosar[:aruIndex]
+#print(darabLista)
+vasarlasDb = darabLista.count("F") + 1
+#print(vasarlasDb)
+
+print("Az első vásárlás sorszáma: " + str(vasarlasDb))
+
+#A termék utolsó indexe
+utolsoIndex = 0
+for i in range(0, len(kosar)):
+    if kosar[i * -1 - 1] == arunev:
+        utolsoIndex = len(kosar)-i
+        break
+darabLista = kosar[:utolsoIndex]
+vasarlasDb = darabLista.count("F") + 1
+print("Az utolsó vásárlás sorszáma: " + str(vasarlasDb))
+
+voltF = False
+szam = 0
+for e in kosar:
+    if e == arunev:
+        if not voltF:
+            szam = szam + 1
+            voltF = True
+    if e == "F":
+        voltF = False
+print(str(szam) + " vásárlás során vettek belőle.")
+
+print(str(vasarlasDb) + " darab vételekor fizetendő: " + str(ertek(vasarlasDb)))
+
+darabF = 0
+elozoIndex = 0
+keresettIndex = 0
+for i in range(0, len(kosar)):
+    if kosar[i] == "F":
+        darabF += 1
+    if darabF == sorszam:
+        elozoIndex = keresettIndex
+        keresettIndex = i
+        break
+
+print(kosar[elozoIndex+1:keresettIndex])
+
+if sorszam > 1:
+    darabKosar = kosar[elozoIndex+1:keresettIndex]
+else:
+    darabKosar = kosar[elozoIndex:keresettIndex]
+
+stat = {}
+for e in darabKosar:
+    if e in stat.keys():
+        stat[e] += 1
+    else:
+        stat[e] = 1
+
+print(stat)
+
+
+
+
+
