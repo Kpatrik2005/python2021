b5d3a8a55bcc899fca9e0eef379e5f436ec246af
Kovács Patrik <kovacsp20@ADY-NAGYATAD.SULINET.HU>
2022-03-23 09:40:13 +0100

    5.feladat

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -1,9 +1,53 @@
+
 f = open("titanic.txt")
 adatok = f.read().split("\n")
-
 f.close()
-print(adatok)
 
-print("2.feladat: "+ str(len(adatok)) + " db")
+print("2. feladat: " + str(len(adatok)) + " db")
 
-print([ int(e.split(";")[1]) + int(e.split(";")[2]) for e in adatok] + " fő")
+print("3. feladat: " + str(sum(int(e.split(";")[1]) + int(e.split(";")[2]) for e in adatok)) + " fő")
+
+ossz=0
+for e in adatok:
+    temp = e.split(";")
+    tulelo = int(temp[1])
+    eltunt = int(temp[2])
+    ossz+=tulelo
+    ossz+=eltunt
+
+kulcsszo = input("4. feladat: Kulcsszó: ")
+van = False
+nincs = True
+#find()
+for e in adatok:
+    if e.find(kulcsszo)>=0:
+        vam=true
+        break
+if van:
+    print("Van találat!")
+else:
+    ("Nincs találat!")
+
+adatok2=[]
+for e in adatok:
+    temp=e.split(";")
+    temp[1]=int(temp[1])
+    temp[2]=int(temp[2])
+talalat=([e for e in adatok if e.find(kulcsszo)>=-1])
+print(talalat)
+if len(talalat)>0:
+    print("Van találat!")
+else:
+    print("Nincs találat!")
+
+print("5.feladat")
+
+
+
+
+
+
+
+
+
+
