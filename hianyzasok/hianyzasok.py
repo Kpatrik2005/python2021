d20023fe8a90d3fd15afdbb74764b38cdd4d5858
Kovács Patrik <kovacsp20@ADY-NAGYATAD.SULINET.HU>
2022-03-29 12:34:42 +0200

    1.feladatfele

---
+++ hianyzasok/hianyzasok.py
@@ -0,0 +1,7 @@
+#beolvasás szerkezet honap na náv
+#lekérdezük a lista méretét
+#a megfelelo napok vizsgálata adot oraszámban az x-ek és i-k vizsgálata
+#list a hiányzásokról
+f = open("naplo.txt")
+
+f.close
