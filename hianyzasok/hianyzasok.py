ead5b90d13c05f92df91c52d80cb752a2cb8fc8e
Kovács Patrik <kovacsp20@ADY-NAGYATAD.SULINET.HU>
2022-04-04 08:45:56 +0200

    3.feladat

--- hianyzasok/hianyzasok.py
+++ hianyzasok/hianyzasok.py
@@ -8,6 +8,7 @@ f.close()
 naplo = []
 honap = 0
 nap = 0
+
 for e in adatok:
     if e[0] == "#":
         honap=e[2:4]
@@ -17,3 +18,33 @@ for e in adatok:
         temp=[]
         temp.append(honap)
         temp.append(nap)
+        vag= e.split(" ")
+        temp.append(" ".join(vag[0:2]))
+        temp.append(vag[2])
+        naplo.append(temp)
+
+print("2.feladatm \nA naploban " + str(len(naplo)) + " hiányzás van.")
+
+igazolt = 0
+for e in naplo:
+    igazolt+=e[3].count("X")
+
+
+igazolatlan = 0
+for e in naplo:
+    igazolatlan+=e[3].count("I")
+print("3.feladat\nAz igazolt hiányzások száma " + str(igazolt) + ", az igazolatlan hiányzások száma " + str(igazolatlan))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
